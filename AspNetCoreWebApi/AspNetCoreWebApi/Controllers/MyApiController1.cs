7a953029fbb9ed05fe61b12d533d7229e2194830
p2972014 <p2972014@yandex.ru>
2022-04-26 22:01:58 +0200

    b1

--- AspNetCoreWebApi/AspNetCoreWebApi/Controllers/MyApiController1.cs
+++ AspNetCoreWebApi/AspNetCoreWebApi/Controllers/MyApiController1.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//b1
 namespace AspNetCoreWebApi.Controllers
 {
     [Route("api/[controller]")]
